10e669c26263a2f111246748c3717557754b0cf4
Adedayo Adeniyi <info@daydah.com>
2016-12-09 02:27:54 +0100

    initial commit

---
+++ paystack.php
@@ -0,0 +1,181 @@
+<?php
+/*------------------------------------------------------------------------
+ # plg_jeemasms_pg_paystack - Plugin Paystack For JEEMASMS
+ #------------------------------------------------------------------------
+ # author    Daydah Concepsts Ltd
+ # copyright Copyright (C) 2016 daydah.com. All Rights Reserved.
+ # @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ # Websites: http://www.daydah.com
+ # Technical Support:  Forum - http://www.daydah.com
+ -------------------------------------------------------------------------*/
+
+/*
+ * ----------------------------------------- CHANGE LOGS -------------------------------------
+ * 
+ * -------------------------------------------------------------------------------------------
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+//import the joomla library file
+jimport('joomla.plugin.plugin');
+
+class plgJeemaSMSPaystack extends JPlugin {
+
+
+  private $_element_name = "paystack";
+  private $_plugin = null;
+  protected $_pluginparams = null;
+  private $_args = null;
+  private $_apps = null;
+  private $_session = null;
+
+  function plgJeemaSMSPaystack(& $subject, $config){
+    $this->_plugin = JPluginHelper::getPlugin( 'jeemasms', 'paystack' );
+    
+    $this->_pluginparams = new JRegistry( $this->_plugin->params );
+    $this->_apps = JFactory::getApplication();
+    $this->_session = JFactory::getSession();
+    
+    parent::__construct($subject, $config);
+  }
+
+  public function onClickBuyNow($args){
+
+    if ($args['element_name'] != $this->_element_name) return;
+    $this->_args = $args;
+
+    if(!$this->checkMandatoryParameters()){
+		$this->redirectBack();
+    }
+    
+    $response = $this->doPayment();
+  }
+
+  private function checkMandatoryParameters(){
+  	$can_proceed = true;
+	if (
+		($this->_args['package_price'] == '') ||
+		($this->_args['package_id'] == '') ||
+		($this->_args['joomla_user_id'] == '') ||
+		($this->_args['payment_tranx_id'] == '') ||
+		($this->_args['payment_id'] == '') ||
+		($this->_args['payment_currency'] == '')
+	){
+		$can_proceed = false;
+	}
+
+	return $can_proceed;
+  }
+
+	private function redirectBack(){
+		$menu = $this->_apps->getMenu();
+		$items   = $menu->getItems('link', 'index.php?option=com_jeemasms&view=smspackagebuy');
+
+		if(isset($items[0])){
+			$itemid = $items[0]->id;
+		}else{
+			$itemid = 0;
+		}
+
+		$msg = JText::_("PLG_PG_GATEWAY_MANDATORY_MISSING");
+		$msg_type='error';
+		
+		$tpl = '';
+		if($this->_args['mobile']=='1'){
+			$tpl='tmpl=component&mobile=1&';
+		}
+		
+		$this->_apps->redirect('index.php?option=com_jeemasms&'.$tpl.'view=smspackagebuy&Itemid='.$itemid,$msg,$msg_type);
+ 	}
+
+  private function doPayment(){
+  	
+  	if($this->_args['mobile']=='1'){ $this->_session->set('ses_mobile',true);	}
+	else{ 		$this->_session->set('ses_mobile',false);  	}
+  	
+	//from paystack.xml
+	$paystack_mode = $this->_pluginparams->get('paystack_live_demo');//paystack demo or live setting. default is demo or zero
+  	$pl_secret_key = $this->_pluginparams->get('paystack_live_secret_key');//paystack live secret key
+  	$pl_public_key = $this->_pluginparams->get('paystack_live_public_key');//paystack live public key
+  	$pt_secret_key = $this->_pluginparams->get('paystack_test_secret_key');//paystack test secret key
+	$pt_public_key = $this->_pluginparams->get('paystack_test_public_key');//paystack test public key
+  	
+	//create the keys
+	if($paystack_mode == 0){
+		$secret_key = $pt_secret_key; $public_key = $pt_public_key;
+	}
+	else{
+		$secret_key = $pl_secret_key; $public_key = $pl_public_key;
+	}
+	
+  	$currency = $this->_args['payment_currency'];
+  	$package_price = $this->_args['package_price'];
+  	$joomla_user_id = $this->_args['joomla_user_id'];
+  	$tranx_id = $this->_args['payment_tranx_id'];
+  	$pack_name = $this->_args['package_name'];
+	
+	//get user email
+	$user = JFactory::getUser(); $email = $user->email;
+	  	
+  	$return_url = JURI::base().'plugins/jeemasms/paystack/paystack/pages/return.php';
+  	//$ipnurl = JURI::base().'plugins/jeemasms/paystack/paystack/pages/ipn.php';
+  	
+	
+  	$session = JFactory::getSession();
+  	$this->_session->set('session_transaction_id',$tranx_id);
+  	
+  	
+	//build the html and javascript form to send out
+  	$tosend = '<p>Your order is being processed. Please wait...</p>
+	<form id="paystack-pay-form" action="' . $return_url . '" method="post">
+  <script src="https://js.paystack.co/v1/inline.js"></script>
+   <button id="paystack-pay-btn" style="display:none" type="button" onclick="payWithPaystack()"> Click here </button>
+          
+        </form>
+ 
+<script>
+        function formatAmount(amount) {
+            var strAmount = amount.toString().split(".");
+            var decimalPlaces = strAmount[1] === undefined ? 0: strAmount[1].length;
+            var formattedAmount = strAmount[0];
+
+            if (decimalPlaces === 0) {
+                formattedAmount += \'00\';
+
+            } else if (decimalPlaces === 1) {
+                formattedAmount += strAmount[1] + \'0\';
+
+            } else if (decimalPlaces === 2) {
+                formattedAmount += strAmount[1];
+            }
+
+            return formattedAmount;
+        }
+       
+        var amount = formatAmount("' . $package_price . '");
+		
+	function payWithPaystack(){
+			var handler = PaystackPop.setup({
+			  key: \'' . $public_key . '\',
+              email: \'' . $email . '\',
+			  amount: amount,
+			  ref: \'' . $tranx_id . '\',
+			  callback: function(response){
+                  document.getElementById(\'paystack-pay-form\').submit();
+              },
+              onClose: function(){
+                  document.getElementById(\'paystack-pay-form\').submit();
+              }
+            });
+            handler.openIframe();
+          }
+          payWithPaystack();
+         
+        </script>';
+        
+        echo $tosend;
+  }
+
+}
---
+++ paystack/pages/return.php
@@ -0,0 +1,194 @@
+<?php
+/*------------------------------------------------------------------------
+ # plg_jeemasms_pg_paystack - Plugin Paystack For JEEMASMS
+ #------------------------------------------------------------------------
+ # author    Daydah Concepsts Ltd
+ # copyright Copyright (C) 2016 daydah.com. All Rights Reserved.
+ # @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ # Websites: http://www.daydah.com
+ # Technical Support:  Forum - http://www.daydah.com
+ -------------------------------------------------------------------------*/
+
+/*
+ * ----------------------------------------- CHANGE LOGS -------------------------------------
+ * 
+ * -------------------------------------------------------------------------------------------
+ */
+
+/* Initialize Joomla framework */
+define( '_JEXEC', 1 );
+define('JPATH_BASE',realpath(dirname(__FILE__).'/../../../../..'));
+define( 'DS', DIRECTORY_SEPARATOR );
+
+/* Required Files */
+require_once (JPATH_BASE .DS.'includes'.DS.'defines.php' );
+require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+
+//import the joomla library file
+jimport( 'joomla.plugin.helper' );
+
+/* Create the Application */
+$mainframe = JFactory::getApplication('site');
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+class PaystackGatewayReturn{
+
+	private $_session = null;
+	private $_responseArray = null;
+	private $_jinput = null;
+	private $_pluginparams = null;
+
+	function __construct(){
+		$this->_session = JFactory::getSession();
+		$this->_jinput = JFactory::getApplication()->input;
+		
+		$plugin = JPluginHelper::getPlugin( 'jeemasms', 'paystack' );
+		$this->_pluginparams = new JRegistry( $plugin->params );
+	}
+	
+	public function getResponse(){
+		$transactionid = $this->_session->get('session_transaction_id');
+	
+		if($transactionid == '')
+		{
+			$this->_responseArray['layout'] = "sessionexpired";
+			$this->_responseArray['gateway_txn_id'] = $this->_jinput->get('transaction_id');
+			$this->_responseArray['payment_tranx_id'] = '';
+			$this->_responseArray['gateway_response'] = "Sorry, your payment session got expired. Please try again in a few minutes.";
+			
+			return $this->_responseArray;
+		}
+		
+		//check if its demo
+		if($this->_pluginparams->get('paystack_live_demo')==0){ $secret_key = $this->_pluginparams->get('paystack_test_secret_key');}
+		else{ $secret_key = $this->_pluginparams->get('paystack_live_secret_key');}
+		
+		//call the function that will create the url and use file get contents to get response from Paystack
+		//returns an array
+		$transData = $this->verifyPaystackTransaction($transactionid, $secret_key);
+		
+		if (!property_exists($transData, 'error') && property_exists($transData, 'status') && ($transData->status === 'success') && (strpos($transData->reference, $transactionid) === 0)) 
+		{
+			// Update order status - From pending to complete
+			$this->_responseArray['layout'] = "success";
+			$this->_responseArray['gateway_txn_id'] = $this->_jinput->get('transaction_id');
+			$this->_responseArray['payment_tranx_id'] = $transactionid;
+			$this->_responseArray['gateway_response'] = "Payment Successfull";
+			return $this->_responseArray;
+		}
+		 
+		 else if (property_exists($transData, 'error')) 
+		 {
+			$this->_responseArray['layout'] = "failure";
+			$this->_responseArray['gateway_txn_id'] = $this->_jinput->get('transaction_id');
+			$this->_responseArray['payment_tranx_id'] = $transactionid;
+			$this->_responseArray['gateway_response'] = "Payment not approved.";
+			return $this->_responseArray;
+		}
+		
+		else
+		{
+			$this->_responseArray['layout'] = "pending";
+			$this->_responseArray['gateway_txn_id'] = $this->_jinput->get('transaction_id');
+			$this->_responseArray['payment_tranx_id'] = $transactionid;
+			$this->_responseArray['gateway_response'] = "Payment status is Pending.";
+			return $this->_responseArray;
+		}
+	}
+
+ private function verifyPaystackTransaction($transactionid, $secret_key){
+	 //$secret_key is either the demo or live secret key from your dashboard.
+	//$transactionid is the transaction reference code sent to the API previously
+	 $transactionStatus        = new stdClass();
+        $transactionStatus->error = "";
+
+        // try a file_get verification
+        $opts = array(
+            'http' => array(
+                'method' => "GET",
+                'header' => "Authorization: Bearer " . $secret_key
+            )
+        );
+
+        $context  = stream_context_create($opts);
+        $url      = "https://api.paystack.co/transaction/verify/" . $transactionid;
+        $response = file_get_contents($url, false, $context);
+
+        // if file_get didn't work, try curl
+        if (!$response) {
+            curl_setopt($ch, CURLOPT_URL, "https://api.paystack.co/transaction/verify/" . $transactionid);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Authorization: Bearer ' . $secret_key
+            ));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+
+            // Make sure CURL_SSLVERSION_TLSv1_2 is defined as 6
+            // cURL must be able to use TLSv1.2 to connect
+            // to Paystack servers
+            if (!defined('CURL_SSLVERSION_TLSv1_2')) {
+                define('CURL_SSLVERSION_TLSv1_2', 6);
+            }
+            curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+            // exec the cURL
+            $response = curl_exec($ch);
+            // should be 0
+            if (curl_errno($ch)) {
+                // curl ended with an error
+                $transactionStatus->error = "cURL said:" . curl_error($ch);
+            }
+            //close connection
+            curl_close($ch);
+        }
+
+        if ($response) {
+            $body = json_decode($response);
+            if (!$body->status) {
+                // paystack has an error message for us
+                $transactionStatus->error = "Paystack API said: " . $body->message;
+            } else {
+                // get body returned by Paystack API
+                $transactionStatus = $body->data;
+            }
+        } else {
+            // no response
+            $transactionStatus->error = $transactionStatus->error . " : No response";
+        }
+
+
+        return $transactionStatus;
+
+ }
+
+ }
+ ?>
+
+<?php
+	echo JText::_('Processing.... Please Wait...');
+
+	$gatewayObj = new PaystackGatewayReturn();
+	$response = $gatewayObj->getResponse();
+	$website_url = JURI::root()."../../../../..";
+?>
+
+<form action="<?php echo JRoute::_($website_url.'/index.php'); ?>" method="POST" name="adminForm" id="adminForm">
+
+<input type="hidden" name="gateway_txn_id" value=<?php echo $response['gateway_txn_id']; ?> />
+<input type="hidden" name="gateway_payment_id" value="<?php echo $response['payment_tranx_id']; ?>" />
+<input type="hidden" name="gateway_response_msg" value="<?php echo $response['gateway_response']; ?>" />
+<input type="hidden" name="gateway_name" value="Paystack" />
+
+<input type="hidden" name="option" value="com_jeemasms" />
+<input type="hidden" name="task" value="" />
+<input type="hidden" name="view" value="paymentgateway" />
+<input type="hidden" name="layout" value="<?php echo $response['layout']; ?>" />
+<input type="hidden" name="boxchecked" value="" />
+<input type="hidden" name="controller" value="" />
+
+</form>
+
+<script language="javascript">
+  document.adminForm.submit();
+</script>
